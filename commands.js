66d029cadf3c734855a39f27617c99c6dd1a6661
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-08-24 19:45:52 +0530

    Change addCustomer from using parameters to inquirer questions

--- commands.js
+++ commands.js
@@ -21,7 +21,7 @@ const questions = [
 	},
 	{
 		type: 'input',
-		name: 'firstname',
+		name: 'email',
 		message: 'Customer Email Address'
 	}
 ];
@@ -30,12 +30,19 @@ program
 	.version('1.0.0')
 	.description('Client Management System');
 
+// program
+// 	.command('add <firstname> <lastname> <phone> <email>')
+// 	.alias('a')
+// 	.description('Add a customer')
+// 	.action((firstname, lastname, phone, email) => {
+// 		addCustomer({ firstname, lastname, phone, email });
+// 	});
 program
-	.command('add <firstname> <lastname> <phone> <email>')
+	.command('add')
 	.alias('a')
 	.description('Add a customer')
-	.action((firstname, lastname, phone, email) => {
-		addCustomer({ firstname, lastname, phone, email });
+	.action(() => {
+		prompt(questions).then(answers => addCustomer(answers));
 	});
 
 program
